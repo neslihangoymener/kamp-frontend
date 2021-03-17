69587d19c70bb03d3f384b9c4ff43d4d0b59fc2c
Neslihan Kuş Göymener <neslihangoymener@gmail.com>
2021-03-17 09:16:12 +0300

    18.ders

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  product1: any = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 };
-  product2: any = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 };
-  product3: any = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5 };
-  product4: any = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 };
-  product5: any = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 };
-
-  products = [
-    this.product1, 
-    this.product2, 
-    this.product3, 
-    this.product4, 
-    this.product5
-  ];
-  constructor() { }
+  products: Product[] = [];
+  dataLoaded = false;
+  
+  constructor(private productService:ProductService, 
+    private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
+      this.activatedRoute.params.subscribe(params=>{
+        if(params["categoryId"]){
+          this.getProductsByCategory(params["categoryId"])
+        }else{
+          this.getProducts()
+        }
+      })
   }
 
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })   
+  }
+
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })   
+  }
 }
