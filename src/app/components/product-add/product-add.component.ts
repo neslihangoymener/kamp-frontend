df6f86dce668aefb4a6ae150fcf0686e85627a31
Neslihan Kuş Göymener <neslihangoymener@gmail.com>
2021-03-24 23:43:36 +0300

    21.gün

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
+import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms";
 import { ToastrService } from 'ngx-toastr';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -13,7 +13,8 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm : FormGroup;
   constructor(private formBuilder:FormBuilder, 
-    private productService:ProductService, private toastrService:ToastrService) { }
+    private productService:ProductService, 
+    private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
